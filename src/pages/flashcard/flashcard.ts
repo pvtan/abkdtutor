063901430c443103fb2722bea2048114153e38b0
Beatriz <mariabeatrizmarasigan@gmail.com>
2018-09-29 22:44:14 +0800

    Button actions in welcome page

--- src/pages/flashcard/flashcard.ts
+++ src/pages/flashcard/flashcard.ts
@@ -8,7 +8,10 @@ import { NavController } from 'ionic-angular';
 export class FlashcardPage {
 
   constructor(public navCtrl: NavController) {
-  	
+
+    window.onload = function() {
+      document.getElementById('imageDiv').innerHTML = '';
+    }
   }
 
 }
