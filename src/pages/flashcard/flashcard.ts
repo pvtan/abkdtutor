15900e202bfd1f17629c6c96313fba4350cfd603
jemmmolina <jhmolina@up.edu.ph>
2018-09-29 18:03:06 +0800

    flashcard added

---
+++ src/pages/flashcard/flashcard.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-flashcard',
+  templateUrl: 'flashcard.html'
+})
+export class FlashcardPage {
+
+  constructor(public navCtrl: NavController) {
+  	
+  }
+
+}
