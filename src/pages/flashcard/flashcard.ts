c293ac3acb8d3243a7d8eb563c25e83e62195de7
Paula Tan <pvtan@up.edu.ph>
2018-09-30 00:33:48 +0800

    Add iterate to next image by voice

--- src/pages/flashcard/flashcard.ts
+++ src/pages/flashcard/flashcard.ts
@@ -9,23 +9,29 @@ declare var annyang: any;
   templateUrl: 'flashcard.html'
 })
 export class FlashcardPage {
-  i: number = 2;
   constructor(public navCtrl: NavController) {
     Speech.init();
     if (annyang) {
      // Let's define a command.
-     var transcription = document.getElementById('speech'); 
+
      var commands = {
-       'hello': function() {
-         console.log("Welcome");
-         transcription.innerHTML = "hello";
-         Speech.speak({
-            text: 'Hello, how are you today ?',
-            onError: (e) => {console.log('sorry an error occurred.', e)}, // optionnal error callback
-            onEnd: () => {console.log('your text has successfully been spoken.')} // optionnal onEnd callback
-          })
+      'a': function() {
+        document.getElementById('symbol').src = "assets/imgs/2.png";
+       },
+       'e': function() {
+        document.getElementById('symbol').src = "assets/imgs/3.png";
+       },
+       'i': function() {
+        document.getElementById('symbol').src = "assets/imgs/4.png";
+       },
+       'o': function() {
+        document.getElementById('symbol').src = "assets/imgs/5.png"; 
+       },
+       'u': function() {
+        document.getElementById('symbol').src = "assets/imgs/6.png";
        }
-     };
+     }; 
+
      annyang.debug(true);
      // Add our commands to annyang
      annyang.addCommands(commands);
@@ -35,13 +41,12 @@ export class FlashcardPage {
    }
 
     window.onload = function() {
-      document.getElementById('imageDiv').innerHTML = '';
+      document.getElementById('imageDiv').innerHTML = ''; 
+      Speech.speak({
+        text: 'a',
+        onError: (e) => {console.log('sorry an error occurred.', e)}, // optionnal error callback
+        onEnd: () => {console.log('your text has successfully been spoken.')} // optionnal onEnd callback
+      })
     }
   }
-
- correct() {
- 	document.getElementById('symbol').src = "assets/imgs/" + this.i + ".png";
- 	this.i++
-  }
-
 }
