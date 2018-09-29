7aef1915891e4583482471d16d264878b4ec60a9
jemmmolina <jhmolina@up.edu.ph>
2018-09-29 23:09:55 +0800

    flashcard next

--- src/pages/flashcard/flashcard.ts
+++ src/pages/flashcard/flashcard.ts
@@ -6,9 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'flashcard.html'
 })
 export class FlashcardPage {
-
+  i: number = 2;
   constructor(public navCtrl: NavController) {
   	
+  }
+
+ correct() {
+ 	document.getElementById('symbol').src = "assets/imgs/" + this.i + ".png";
+ 	this.i++
   }
 
 }
