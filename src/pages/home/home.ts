dc7f171c89bea45e40b4349acc2fe019711c7608
Belle David <princessbelleariedl@gmail.com>
2018-09-30 02:56:30 +0800

    AEIOU to ABKD

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,32 +11,6 @@ declare var annyang: any;
 export class HomePage {
   
   constructor(public navCtrl: NavController) {
-
-      window.onload = function() {
-      Speech.init();
-      if (annyang) {
-       // Let's define a command.
-       var transcription = document.getElementById('speech');
-       var commands = {
-         'hello': function() {
-           console.log("Welcome");
-           transcription.innerHTML = "hello";
-           Speech.speak({
-              text: 'Hello, how are you today ?',
-              onError: (e) => {console.log('sorry an error occurred.', e)}, // optionnal error callback
-              onEnd: () => {console.log('your text has successfully been spoken.')} // optionnal onEnd callback
-          })
-         }
-       };
-       annyang.debug(true);
-       // Add our commands to annyang
-       annyang.addCommands(commands);
-       annyang.setLanguage("fil-PH");
-       // Start listening.
-       annyang.start({continuous: false});
-     }
-
-      };
   }
 
 
