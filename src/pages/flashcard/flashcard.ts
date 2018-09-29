5e4b0dbce1afabb9966eeaf1b1923cdb079d4e49
Paula Tan <pvtan@up.edu.ph>
2018-09-29 23:44:07 +0800

    Transfer text-to-speech and speech recognition to flashcard

--- src/pages/flashcard/flashcard.ts
+++ src/pages/flashcard/flashcard.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { annyang } from 'annyang';
+import Speech from 'speak-tts'
+declare var annyang: any;
 
 @Component({
   selector: 'page-flashcard',
@@ -8,6 +11,28 @@ import { NavController } from 'ionic-angular';
 export class FlashcardPage {
   i: number = 2;
   constructor(public navCtrl: NavController) {
+    Speech.init();
+    if (annyang) {
+     // Let's define a command.
+     var transcription = document.getElementById('speech'); 
+     var commands = {
+       'hello': function() {
+         console.log("Welcome");
+         transcription.innerHTML = "hello";
+         Speech.speak({
+            text: 'Hello, how are you today ?',
+            onError: (e) => {console.log('sorry an error occurred.', e)}, // optionnal error callback
+            onEnd: () => {console.log('your text has successfully been spoken.')} // optionnal onEnd callback
+          })
+       }
+     };
+     annyang.debug(true);
+     // Add our commands to annyang
+     annyang.addCommands(commands);
+     annyang.setLanguage("fil-PH");
+     // Start listening.
+     annyang.start({continuous: false});
+   }
 
     window.onload = function() {
       document.getElementById('imageDiv').innerHTML = '';
